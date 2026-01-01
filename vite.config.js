499ea046e2b0f39bc359995636b287b83a2febe6
deivid.garcia.garcia <deivid.garcia.garcia@gmail.com>
2026-01-01 20:05:10 +0100

    Configure base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // For local development, use '/'
-  // For GitHub Pages, change to '/your-repo-name/'
+  // For GitHub Pages, use '/repo-name/'
   // For custom domain, use '/'
-  base: '/',
+  base: '/ecoempapador/',
 });
